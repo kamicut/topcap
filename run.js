41484781545b1f25f82b767512177b66fce8a1cb
Marc Farra <marcfarra@gmail.com>
2013-12-09 00:17:42 +0200

    Fix undefined data error

--- run.js
+++ run.js
@@ -2,6 +2,9 @@ var config 	= require('./config.js')
 var Topcap 	= require('./Topcap.js')
 
 var tc = new Topcap(config);
-tc.on('data', function() {
+tc.on('data', function(data) {
 	console.log(data)
+})
+tc.on('error', function(error) {
+	console.log(error)
 })
