1c8d20956bcfb988f98c4502c7b9ad7eaee93bc6
Marc Farra <marcfarra@gmail.com>
2014-01-22 17:36:48 -0500

    Update config.js

--- example/config.js
+++ example/config.js
@@ -1,6 +1,6 @@
 var config = {
 	sheet: "Sheet1",
-	interval: 5000,
+	interval: 20000,
 	scriptURL: "https://script.google.com/macros/s/" + 
 		"AKfycbxQiAhRgneNbagmZwHJ6RFrJiJtFA8IMutdzI7IPh_Bbi5DTqGw/exec"
 }
